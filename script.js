cb63dc85d8330b5a1252e04c16cd9ef34432bcba
ujjawaltyagii <ujjawal2002tyagi@gmail.com>
2024-05-01 17:11:50 +0530

    update 6.0

--- script.js
+++ script.js
@@ -95,3 +95,17 @@ function videoAnimation(){
 }
 
 videoAnimation();
+
+var sec = document.querySelectorAll(".s2right");
+
+sec.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        elem.childNodes[3].style.opacity = 1;
+        elem.childNodes[3].play();
+    })
+
+    elem.addEventListener("mouseleave", function(){
+        elem.childNodes[3].style.opacity = 0;
+        elem.childNodes[3].load();
+    })
+})
